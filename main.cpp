6256a6bd526b8240720f1942627b536d3e83e55a
FJ98 <felix.solano@utec.edu.pe>
2019-10-03 22:04:41 -0500

    pequenas pruebas

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main() {
 
-    std::string equations[] = { "7/4*((3+1)*3)+3", "7/4*((2+8)*2)+3", "2+(3)", "7/4*((1+2)^4*2)+3",
-                           "7*1", "1-2*5/2+1-2^4+1", "((7*3)/4+6*(3^2)/2)*(1)", "(5+3)+(21/3*5)-(5^3-2)" };
-    float results[] = { 24, 38, 5, 286.5, 7, -18, 32.25, -80 };
+    std::string equations[] = { "7/4*((3+1)*3)+3", "2+(3)", "7/4*((1+2)^4*2)+3",
+                                "1-2*5/2+1-2^4+1", "((7*3)/4+6*(3^2)/2)" };
+    float results[] = { 24, 5, 286.5, -18, 32.25 };
     const unsigned int size = sizeof(equations) / sizeof(string);
     for (int i = 0; i < static_cast<int>(size); ++i) {
         cout << "Equation -> " << i << endl;
